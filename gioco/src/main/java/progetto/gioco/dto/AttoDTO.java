c7b3b6682506d5458edb86db710978ba40c519e8
GiuseppeSifanno <90522347+GiuseppeSifanno@users.noreply.github.com>
2026-04-26 11:47:42 +0200

    Aggiunte classi DTO per Atto, Dialogo e Scelta

---
+++ gioco/src/main/java/progetto/gioco/dto/AttoDTO.java
@@ -0,0 +1,9 @@
+package progetto.gioco.dto;
+
+import java.util.List;
+
+public class AttoDTO {
+    public String idAtto;
+    public String dialogoIniziale;
+    public List<DialogoDTO> dialoghi;
+}
---
+++ gioco/src/main/java/progetto/gioco/dto/SceltaDTO.java
@@ -0,0 +1,7 @@
+package progetto.gioco.dto;
+
+public class SceltaDTO {
+    public String id;
+    public String testo;
+    public String next;
+}
