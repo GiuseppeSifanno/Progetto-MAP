08ca92921f3895b86c330b2e39955537d24d816b
GiuseppeSifanno <90522347+GiuseppeSifanno@users.noreply.github.com>
2026-04-27 21:50:33 +0200

    Aggiunto parametro `nome` a `AbstractEntity` e `NPC`, implementato metodo `toString` e aggiornato `getDialogo` per restituire il dialogo iniziale

--- gioco/src/main/java/progetto/gioco/model/AbstractEntity.java
+++ gioco/src/main/java/progetto/gioco/model/AbstractEntity.java
@@ -5,7 +5,17 @@ public class AbstractEntity {
     private String nome;
     private final String dialogoIniziale;
 
-    public AbstractEntity(String dialogoIniziale) {
+    public AbstractEntity(String dialogoIniziale, String nome) {
         this.dialogoIniziale = dialogoIniziale;
+        this.nome = nome;
+    }
+    public String getDialogoIniziale() {
+        return dialogoIniziale;
+    }
+
+    //restituisce il nome dell'NPC
+    @Override
+    public String toString() {
+        return nome;
     }
 }
--- gioco/src/main/java/progetto/gioco/model/NPC.java
+++ gioco/src/main/java/progetto/gioco/model/NPC.java
@@ -1,13 +1,15 @@
 package progetto.gioco.model;
 
+import progetto.gioco.controller.GameState;
+
 public class NPC extends AbstractEntity{
-    public NPC(String dialogoIniziale) {
-        super(dialogoIniziale);
+    public NPC(String dialogoIniziale, String nome) {
+        super(dialogoIniziale, nome);
     }
 
     public String getDialogo(GameState state){
         //in base allo stato del gioco (quindi decisioni ecc...) si restituisce il giusto id
         //per far proseguire la storia
-        return "";
+        return getDialogoIniziale();
     }
 }
