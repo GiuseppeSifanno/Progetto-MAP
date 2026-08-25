c8c54049520d5a2e466fe952d950d8d1614c5f07
GiuseppeSifanno <90522347+GiuseppeSifanno@users.noreply.github.com>
2026-08-25 11:33:21 +0200

    Aggiunto pannello inventario con layout dinamico e gestione oggetti, integrato in GestoreSchermate con overlay e sfondo scurito. Refactor delle dipendenze di GameManager nelle classi GUI.

--- gioco/src/main/java/game/gui/BasePanel.java
+++ gioco/src/main/java/game/gui/BasePanel.java
@@ -1,13 +1,14 @@
 package game.gui;
 
 import engine.manager.BaseGameManager;
+import game.manager.GameManager;
 
 import javax.swing.JPanel;
 
 public abstract class BasePanel extends JPanel {
-    private final BaseGameManager gameManager;
+    protected final GameManager gameManager;
 
-    public BasePanel(BaseGameManager gameManager) {
+    public BasePanel(GameManager gameManager) {
         this.gameManager = gameManager;
     }
     public abstract void init();
